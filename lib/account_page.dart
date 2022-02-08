e73a7ef4d5798e040a527655c27fef8090d95ce3
xamata <61128570+xamata@users.noreply.github.com>
2022-02-07 21:47:27 -0800

    Changing the Sign in page layout for a more aesthetic look to please the eyes

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -12,98 +12,116 @@ class _AccountPageState extends State<AccountPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
       body: Stack(
+        // alignment: AlignmentDirectional.topStart,
         fit: StackFit.expand,
         children: [
-          Image(
-            image: const AssetImage('images/background.jpeg'),
-            fit: BoxFit.cover,
+          const Image(
+              image: AssetImage('images/background.jpeg'),
+              fit: BoxFit.fill,
+              height: 200
 
-            //     .cover, //as small as possible while still covering the entire target box
-            // colorBlendMode: BlendMode.darken,
-            // color: Colors.black54,
-          ),
+              //     .cover, //as small as possible while still covering the entire target box
+              // colorBlendMode: BlendMode.darken,
+              // color: Colors.black54,
+              ),
           Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Image.asset(
-                  'images/test_image.jpeg',
-                  height: 50,
-                  width: 50,
+              const Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(
+                      fontFamily: "RaleWay",
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              Stack(children: <Widget>[
-                SingleChildScrollView(
-                  child: Container(
-                    height: 300,
-                    width: 300,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30, vertical: 25),
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(20),
-                      shape: BoxShape.rectangle,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextField(
-                          autocorrect: false,
-                          autofocus: false,
-                          style: const TextStyle(fontSize: 18),
-                          decoration: InputDecoration(
-                              hintText: 'UserName',
-                              border: InputBorder.none,
-                              filled: true,
-                              fillColor: Colors.grey[200],
-                              contentPadding: const EdgeInsets.all(10.0),
-                              hoverColor: Colors.indigo),
-                          cursorColor: Colors.purple,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 25,
-                          ),
-                          child: TextField(
-                            autocorrect: false,
-                            autofocus: false,
-                            obscureText: false,
-                            style: const TextStyle(fontSize: 18),
-                            decoration: InputDecoration(
-                              hintText: "Password",
-                              filled: true,
-                              fillColor: Colors.grey[200],
-                              contentPadding: const EdgeInsets.all(10.0),
-                            ),
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: const [
-                            Text(
-                              'Forgot Password?',
-                              style: TextStyle(color: Colors.red, fontSize: 14),
-                            )
-                          ],
-                        ),
-                        RawMaterialButton(
-                          onPressed: () {},
-                          constraints: const BoxConstraints(minWidth: 200),
-                          splashColor: Colors.green,
-                          fillColor: Colors.red,
-                          padding: const EdgeInsets.symmetric(vertical: 12),
-                          child: const Text(
-                            "Login",
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                        )
-                      ],
-                    ),
+              const Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, bottom: 30),
+                child: Text(
+                  "Become a member to enjoy exclusive savings on your favorite items.",
+                  style: TextStyle(
+                    fontSize: 16,
                   ),
-                )
-              ])
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              // Stack(children: <Widget>[
+              //   SingleChildScrollView(
+              //     child: Container(
+              //       height: 300,
+              //       width: 300,
+              //       padding: const EdgeInsets.symmetric(
+              //           horizontal: 30, vertical: 25),
+              //       decoration: BoxDecoration(
+              //         color: Colors.blue,
+              //         borderRadius: BorderRadius.circular(20),
+              //         shape: BoxShape.rectangle,
+              //       ),
+              //       child: Column(
+              //         mainAxisAlignment: MainAxisAlignment.center,
+              //         children: [
+              //           TextField(
+              //             autocorrect: false,
+              //             autofocus: false,
+              //             style: const TextStyle(fontSize: 18),
+              //             decoration: InputDecoration(
+              //                 hintText: 'UserName',
+              //                 border: InputBorder.none,
+              //                 filled: true,
+              //                 fillColor: Colors.grey[200],
+              //                 contentPadding: const EdgeInsets.all(10.0),
+              //                 hoverColor: Colors.indigo),
+              //             cursorColor: Colors.purple,
+              //           ),
+              //           Padding(
+              //             padding: const EdgeInsets.symmetric(
+              //               vertical: 25,
+              //             ),
+              //             child: TextField(
+              //               autocorrect: false,
+              //               autofocus: false,
+              //               obscureText: false,
+              //               style: const TextStyle(fontSize: 18),
+              //               decoration: InputDecoration(
+              //                 hintText: "Password",
+              //                 filled: true,
+              //                 fillColor: Colors.grey[200],
+              //                 contentPadding: const EdgeInsets.all(10.0),
+              //               ),
+              //             ),
+              //           ),
+              //           Row(
+              //             mainAxisAlignment: MainAxisAlignment.end,
+              //             children: const [
+              //               Text(
+              //                 'Forgot Password?',
+              //                 style: TextStyle(color: Colors.red, fontSize: 14),
+              //               )
+              //             ],
+              //           ),
+              //           RawMaterialButton(
+              //             onPressed: () {},
+              //             constraints: const BoxConstraints(minWidth: 200),
+              //             splashColor: Colors.green,
+              //             fillColor: Colors.red,
+              //             padding: const EdgeInsets.symmetric(vertical: 12),
+              //             child: const Text(
+              //               "Login",
+              //               style: TextStyle(color: Colors.white, fontSize: 15),
+              //             ),
+              //           )
+              //         ],
+              //       ),
+              //     ),
+              //   )
+              // ])
             ],
           ),
         ],
